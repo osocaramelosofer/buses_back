ad2092c4ce7d4b977804b0f64c89b1681b46bf71
osocaramelosofer <osocaramelosofer@hotmail.com>
2023-02-26 10:41:58 -0600

    Revert "Add serializers"
    This reverts commit 71a683f3b4d7ae1ded3e2f8377dd107dde46498e.

--- busses_backend/busses/models.py
+++ busses_backend/busses/models.py
@@ -3,11 +3,10 @@ from django.db import models
 
 class Route(models.Model):
     name = models.CharField(max_length=200)
-    origin = models.FloatField()
     duration = models.FloatField()
     destination = models.CharField(max_length=100)
-    departure_date = models.DateTimeField()
-    arrival_date = models.DateTimeField()
+    date_departure = models.DateTimeField()
+    date_arrival = models.DateTimeField()
 
     def __str__(self):
         return self.name
@@ -38,11 +37,11 @@ class BusAssignment(models.Model):
     driver = models.ForeignKey(
         Driver, on_delete=models.CASCADE, related_name="bus_assignment"
     )
-    departure_date = models.DateTimeField()
-    arrival_date = models.DateTimeField()
+    date_departure = models.DateTimeField()
+    date_arrival = models.DateTimeField()
 
     def __str__(self):
-        return f"{self.route.name} - {self.bus.serial_number}"
+        return f"{self.route.name} - {self.bus.serial_number} - {self.schedule}"
 
 
 class Passenger(models.Model):
