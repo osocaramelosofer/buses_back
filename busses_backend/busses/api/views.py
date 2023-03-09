0dd48ea2c530802f1fafe9f5e80efedfbb2176c9
osocaramelosofer <osocaramelosofer@hotmail.com>
2023-03-09 11:33:56 -0600

    Resolving pre-commit-hook changes

--- busses_backend/busses/api/views.py
+++ busses_backend/busses/api/views.py
@@ -76,34 +76,8 @@ class CreateBoletoGenericApiView(
     queryset = Boleto.objects.all()
 
     def get_serializer_class(self, *args, **kwargs):
-        if self.request.method == "post":
-            print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX 1", self.request.method)
-            return FullBoletosSerializer
         if self.request.method == "POST":
-            print(
-                "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX 2",
-                self.request.method,
-            )
             return FullBoletosSerializer
-        if self.request.method == "put":
-            print(
-                "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX 3",
-                self.request.method,
-            )
-            return FullBoletosSerializer
-        if self.request.method == "PUT":
-            print(
-                "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX 4",
-                self.request.method,
-            )
-            return FullBoletosSerializer
-        if self.request.action == "create":
-            print(
-                "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX 5",
-                self.request.method,
-            )
-            return FullBoletosSerializer
-        # return BoletoSerializer
         return BoletoSerializer.default
 
     def post(self, request, *args, **kwargs):
